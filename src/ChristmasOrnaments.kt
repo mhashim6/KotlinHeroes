d5837484984a1a83a2ca07a4c2b2182153063546
mhashim6 <m6hashim@gmail.com>
2019-05-31 22:31:34 +0200

    a better christmas.

--- src/ChristmasOrnaments.kt
+++ src/ChristmasOrnaments.kt
@@ -1,6 +1,6 @@
 /**
 URL: https://codeforces.com/contest/1171/problem/B
-*/
+ */
 
 /**
  *@author mhashim6 on 27/05/19
@@ -8,12 +8,9 @@ URL: https://codeforces.com/contest/1171/problem/B
 
 
 fun main() {
-    var y = 0;
-    var b = 0;
-    var r = 0;
-    with(readLine()!!.split(" ")) {
-        y = this[0].toInt(); b = this[1].toInt(); r = this[2].toInt()
-    }
+    var (y, b, r) = readLine()!!.split(" ").map(String::toInt).toTriple()
     while (y + 1 > b || y + 2 > r) y--
     println(3 * y + 3)
 }
+
+fun <T> List<T>.toTriple(i: Int = 0, j: Int = 1, k: Int = 2) = Triple(this[i], this[j], this[k])
